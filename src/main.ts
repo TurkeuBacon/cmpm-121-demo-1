4b1e047b1b367cc753491de79b04d334224ebb43
TurkeuBacon <99289161+TurkeuBacon@users.noreply.github.com>
2023-12-09 13:25:04 -0800

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,41 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My game";
+const gameName = "Goofy Ah Guy Collector";
+const GOOFY_AH_GUY = "🤓☝️";
+
+let GoofyAhCounter: number = 0;
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 
-const button = document.createElement("button");
-button.innerText = "🤓";
+const goofyAhGuysButton = document.createElement("button");
+goofyAhGuysButton.innerText = GOOFY_AH_GUY;
+goofyAhGuysButton.onclick = () => {
+  GoofyAhCounter++;
+  goofyAhGuysLabel.innerText = getLabelText();
+};
+
+const goofyAhGuysLabel = document.createElement("div");
+goofyAhGuysLabel.innerText = getLabelText();
 
 app.append(header);
-app.append(button);
+app.append(goofyAhGuysButton);
+app.append(goofyAhGuysLabel);
+
+function getLabelText(): string {
+  let text: string =
+    "You have " +
+    GoofyAhCounter +
+    " Goofy Ah Guy" +
+    (GoofyAhCounter != 1 ? "s" : "");
+
+  const exclamations = GoofyAhCounter > 0 ? Math.log(GoofyAhCounter) : 0;
+  for (let i = 0; i < exclamations; i++) {
+    text += "!";
+  }
+
+  return text;
+}
