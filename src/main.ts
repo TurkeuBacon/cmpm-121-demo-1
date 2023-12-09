41e590907f5a673eb5139a0b1bae484c07f5538a
TurkeuBacon <99289161+TurkeuBacon@users.noreply.github.com>
2023-12-09 12:57:56 -0800

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,9 @@ document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
+
+const button = document.createElement("button");
+button.innerText = "🤓";
+
 app.append(header);
+app.append(button);
